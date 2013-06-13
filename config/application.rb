6168b43cbdf5ff71d70d44fc50387092e337a118
Chris Missal <chris.missal@gmail.com>
2013-06-12 22:11:05 -0500

    added www_ditcher

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module KevinSpacer
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.middleware.use "WwwDitcher"
+
   end
 end
